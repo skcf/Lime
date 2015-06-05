949fad0e99f24589f0153e05747565a40f305da9
Souichi <sk.cf.msc@gmail.com>
2015-06-05 23:05:29 +0900

    Initial Commit

---
+++ lime.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Lime"
+	app.Version = Version
+	app.Usage = ""
+	app.Author = "Souichi"
+	app.Email = "sk.cf.msc@gmail.com"
+	app.Action = doMain
+	app.Run(os.Args)
+}
+
+func doMain(c *cli.Context) {
+	println ("hello world")
+}
