89c0a333c18452595d0488e88533d85fd9d6bd45
Souichi <sk.cf.msc@gmail.com>
2015-06-19 10:02:42 +0900

    Add lime ls [template] command

--- commands.go
+++ commands.go
@@ -127,31 +127,44 @@ func removeFile(file string) {
 }
 
 func doLs(c *cli.Context) {
-    home := os.Getenv("HOME")
-    targetPath := home + "/.lime"
+    switch {
+        case len(c.Args()) == 0:
+            home := os.Getenv("HOME")
+            targetPath := home + "/.lime"
 
-    var templates []string
-    var scripts []string
+            var templates []string
+            var scripts []string
 
-    files, _ := ioutil.ReadDir(targetPath)
-    for _, f := range files {
-        if f.IsDir() == true {
-          templates = append(templates,f.Name())
-        } else {
-          scripts = append(scripts,f.Name())
-        }
+            files, _ := ioutil.ReadDir(targetPath)
+            for _, f := range files {
+                if f.IsDir() == true {
+                  templates = append(templates,f.Name())
+                } else {
+                  scripts = append(scripts,f.Name())
+                }
+            }
+
+            fmt.Println("\n*** Lime Template Directories ***")
+            for _, i := range templates {
+                fmt.Println(i)
+            }
+
+            fmt.Println("\n*** Lime Template Scripts ***")
+            for _, i := range scripts {
+                fmt.Println(i)
+            }
+            fmt.Println("")
+        case len(c.Args()) == 1:
+            home := os.Getenv("HOME")
+            targetPath := home + "/.lime/" + c.Args()[0]
+
+            fmt.Println("\n*** Lime Template " + c.Args()[0] + " ***")
+
+            files, _ := ioutil.ReadDir(targetPath)
+            for _, f := range files {
+                fmt.Println(f.Name())
+            }
     }
-
-    fmt.Println("\n*** Lime Template Directories ***")
-    for _, i := range templates {
-        fmt.Println(i)
-    }
-
-    fmt.Println("\n*** Lime Template Scripts ***")
-    for _, i := range scripts {
-        fmt.Println(i)
-    }
-    fmt.Println("")
 }
 
 func doSetup(c *cli.Context) {
