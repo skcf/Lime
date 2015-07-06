71a4423108d0a3b46222d30f04870332aab4572c
Souichi <sk.cf.msc@gmail.com>
2015-07-07 01:04:39 +0900

    Copy Templates Dir by setup command

--- setup.go
+++ setup.go
@@ -15,12 +15,28 @@ var commandSetup = cli.Command{
 
 func doSetup(c *cli.Context) {
     home := os.Getenv("HOME")
-    targetPath := home + "/.lime"
-    cmd := exec.Command("mkdir",targetPath)
-    stdout, err := cmd.Output()
-    if err != nil {
-        println(err.Error())
-        return
-    }
-    println(string(stdout))
+		if exist(home+"/.lime") {
+			copyTemplates()
+		} else {
+				targetPath := home + "/.lime"
+				cmd := exec.Command("mkdir",targetPath)
+				stdout, err := cmd.Output()
+				if err != nil {
+				    println(err.Error())
+				    return
+				}
+				println(string(stdout))
+				copyTemplates()
+		}
+}
+
+func copyTemplates() {
+	home := os.Getenv("HOME")
+	limePath := home + "/.lime/"
+	templatePath := os.Getenv("GOPATH") + "/src/github.com/skcf/Lime/Templates"
+	cmd := exec.Command("cp","-r",templatePath,limePath)
+	_, err := cmd.Output()
+	if err != nil {
+			println(err.Error())
+	}
 }
